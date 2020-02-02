5c3ca85b8d0fb1c95de4ba88a7f117575f97fa0e
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-02-02 09:49:49 +0100

    Data is feching from the Api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import { Route, Switch, Link } from "react-router-dom";
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage.js";
+import SearchForm from "./components/SearchForm";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <SearchForm />
+      <Switch>
+        <Route exart path='/'>
+          <WelcomePage />
+        </Route>
+        <Route exact path='/component'>
+          <CharacterList />
+        </Route>
+      </Switch>
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,46 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function CharacterList() {
+function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const getCharacters = () => {
+      // TODO: Add API Request here - must run in `useEffect`
+      //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+      axios
+        .get("https://rickandmortyapi.com/api/character/")
+        .then(response => {
+          setCharacters(response.data.results);
+          console.log(response);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    };
+    getCharacters();
   }, []);
+  console.log(characters);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+    <section className='character-list'>
+      <div>
+        {characters.map(character => {
+          return (
+            <div key={character.id}>
+              <img scr={character.image} />
+              <h2>Name :{character.name}</h2>
+              <p> Status :{character.status} </p>
+              <p>Species :{character.species}</p>
+              const names = {character.name}
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
+
+export default CharacterList;
